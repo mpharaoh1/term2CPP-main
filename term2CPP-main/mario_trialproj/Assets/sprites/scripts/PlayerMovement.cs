fe67feae2703efdd7c7eab80b8b3a294ce983218
mpharaoh1 <marckomina211@gmail.com>
2021-04-23 16:29:34 -0400

    Initial commit

---
+++ term2CPP-main/mario_trialproj/Assets/sprites/scripts/PlayerMovement.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator), typeof(SpriteRenderer))]
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator anim;
+    SpriteRenderer marioSprite;
+
+    public float speed;
+    public int jumpForce;
+    public bool isGrounded;
+    public bool isAttacking;
+    public LayerMask isGroundLayer;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        marioSprite = GetComponent<SpriteRenderer>();
+
+        if (speed <= 0)
+        {
+            speed = 5.0f;
+        }
+
+        if (jumpForce <= 0)
+        {
+            jumpForce = 300;
+        }
+
+        if (groundCheckRadius <= 0)
+        {
+            groundCheckRadius = 0.2f;
+        }
+
+        if (!groundCheck)
+        {
+            Debug.Log("Groundcheck does not exist, please assign a ground check object");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Input.GetButton("Fire2"))
+        {
+
+            anim.SetBool("isAttacking", false);
+            isAttacking = false;
+        }
+        else 
+        {
+            anim.SetBool("isAttacking", true);
+            isAttacking = true;
+        }
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, isGroundLayer);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+
+        Vector2 moveDirection = new Vector2(horizontalInput * speed, rb.velocity.y);
+        rb.velocity = moveDirection;
+
+        anim.SetFloat("speed", Mathf.Abs(horizontalInput));
+        anim.SetBool("isGrounded", isGrounded);
+
+        if (marioSprite.flipX && horizontalInput > 0 || !marioSprite.flipX && horizontalInput < 0)
+            marioSprite.flipX = !marioSprite.flipX;
+
+    }
+}
